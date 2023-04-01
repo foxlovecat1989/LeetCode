3a33f82d7cfde3faf50e59f241b37d4019e3cb8a
chenhuangkai <huang.kai@tpisoftware.com>
2023-04-01 09:07:24 +0800

    init project

---
+++ src/test/java/com/ed/leetcode/LeetCodeApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ed.leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class LeetCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
